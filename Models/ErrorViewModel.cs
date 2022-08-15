596186416a0f46c91d56768c95d3f141080eaa3d
Mary Giles Beeson <marybeeson23@gmail.com>
2022-08-15 10:50:37 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinancialTrackerMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
